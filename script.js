555f8133e9f82f43ed703f18f03403246542b15e
Galaluang <kwang.nrsr@gmail.com>
2026-04-20 15:21:40 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+// Elements
+const envelope = document.getElementById("envelope-container");
+const letter = document.getElementById("letter-container");
+const noBtn = document.querySelector(".no-btn");
+const yesBtn = document.querySelector(".btn[alt='Yes']");
+
+const title = document.getElementById("letter-title");
+const catImg = document.getElementById("letter-cat");
+const buttons = document.getElementById("letter-buttons");
+const finalText = document.getElementById("final-text");
+
+// Click Envelope
+
+envelope.addEventListener("click", () => {
+    envelope.style.display = "none";
+    letter.style.display = "flex";
+
+    setTimeout( () => {
+        document.querySelector(".letter-window").classList.add("open");
+    },50);
+});
+
+// Logic to move the NO btn
+
+let clickCount = 0;
+
+noBtn.addEventListener("click", () => {
+    clickCount++;
+
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+
+    const distance = Math.min(screenWidth, screenHeight) * 0.3;
+
+    const angle = Math.random() * Math.PI * 2;
+
+    // 👇 ใส่ตรงนี้
+    const maxX = screenWidth / 2 - noBtn.offsetWidth;
+    const maxY = screenHeight / 2 - noBtn.offsetHeight;
+
+    const moveX = Math.cos(angle) * Math.min(distance, maxX);
+    const moveY = Math.sin(angle) * Math.min(distance, maxY);
+
+    noBtn.style.transition = "transform 0.3s ease";
+    noBtn.style.transform = `translate(${moveX}px, ${moveY}px)`;
+
+    if (clickCount === 5) {
+        catImg.src = "sad.gif";
+    }
+});
+// Logic to make YES btn to grow
+
+// let yesScale = 1;
+
+// yesBtn.style.position = "relative"
+// yesBtn.style.transformOrigin = "center center";
+// yesBtn.style.transition = "transform 0.3s ease";
+
+// noBtn.addEventListener("click", () => {
+//     yesScale += 2;
+
+//     if (yesBtn.style.position !== "fixed") {
+//         yesBtn.style.position = "fixed";
+//         yesBtn.style.top = "50%";
+//         yesBtn.style.left = "50%";
+//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+//     }else{
+//         yesBtn.style.transform = `translate(-50%, -50%) scale(${yesScale})`;
+//     }
+// });
+
+// YES is clicked
+
+yesBtn.addEventListener("click", () => {
+
+    catImg.src = "cat_dance.gif";
+
+    document.querySelector(".letter-window").classList.add("final");
+
+    buttons.style.display = "none";
+
+    finalText.style.display = "block";
+
+    const song = document.getElementById("loveSong");
+song.volume = 0;
+song.play();
+
+let vol = 0;
+const fade = setInterval(() => {
+    if (vol < 1) {
+        vol += 0.05;
+        song.volume = vol;
+    } else {
+        clearInterval(fade);
+    }
+}, 200);
+
+
+});
